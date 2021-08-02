d860aefdfdbef034d80253f0eb04e8fbc41a32ed
Andrey_Gravshin <andrey_gravshin@epam.com>
2021-08-02 14:51:31 +0300

    notify10

--- src/com/epam/edp/stages/impl/cd/impl/Deploy.groovy
+++ src/com/epam/edp/stages/impl/cd/impl/Deploy.groovy
@@ -28,7 +28,7 @@ class Deploy {
         }
         return true
     }
-
+echo1 111
     def getBuildUserFromLog(context) {
         def jenkinsCred = "admin:${context.jenkins.token}".bytes.encodeBase64().toString()
         def jobUrl = "${context.job.buildUrl}".replaceFirst("${context.job.jenkinsUrl}", '')
